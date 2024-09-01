596ff4e1f3edb36d6602013c10efc9fec31e72eb
Ryxjx YxBx <51150175+YABER1965@users.noreply.github.com>
2024-09-01 17:06:03 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,13 @@
 # ---
 from fasthtml.common import *
-#import uvicorn
+import uvicorn
 
-# ---
-page = Html(
-            Head(
-                Meta(charset='UTF-8'),
-                Meta(name='viewport', content='width=device-width, initial-scale=1'),
-                Title('LPテンプレート'),
-                Meta(name='keywords', content=''),
-                Meta(name='description', content=''),
-                Link(rel='icon', type='image/png', href='img/favicon.png')
-            ),
-            Body(
-                Header(
-                    Div(cls='row')(
-                        H1(
-                            A('LOGO')
-                        )
-                    )
-                ),
-                Div(cls='mainimg')(
-                    Img(src='img/mainimg.jpg', alt='メイン画像')
-                ),
-                Main(
-                    Div(cls='catch')(
-                        H2(
-                            Span('キャッチフレーズ', cls='under')
-                        ),
-                        P(
-                            'ここにキャッチコピーが入ります。',
-                            Br(),
-                            'お店の宣伝やコンセプトなど。魅せる文言を入力。'
-                        )
-                    ),
-                    Section(id='1', cls='gray-back')(
-                        H2(cls='center')(
-                            Span('商品について', cls='under')
-                        )
-                    ),
-                    Section(id='2')(
-                        H2(cls='center')(
-                            Span('スタッフ紹介', cls='under')
-                        )
-                    ),
-                    Section(id='3', cls='gray-back')(
-                        H2(cls='center')(
-                            Span('申し込みの流れ', cls='under')
-                        )
-                    ),
-                    Section(id='4')(
-                        H2(cls='center')(
-                            Span('アクセス', cls='under')
-                        )
-                    ),
-                    Section(id='5')(
-                        H2(cls='center')(
-                            Span('お問い合わせ', cls='under')
-                        )
-                    )
-                ),
-                Footer(
-                    Div(cls='row')(
-                        Div(cls='col span-4')(
-                            H4('フッター1'),
-                            P('ここにSNSやテキストなどが入ります。SNSやテキストなどが入ります。')
-                        ),
-                        Div(cls='col span-4')(
-                            H4('フッター2'),
-                            P('ここにSNSやテキストなどが入ります。SNSやテキストなどが入ります。')
-                        ),
-                        Div(cls='col span-4')(
-                            H4('フッター3'),
-                            P('ここにSNSやテキストなどが入ります。SNSやテキストなどが入ります。')
-                        )
-                    )
-                )
-            )
-        )
+app = FastHTML()
+rt = app.route
 
-# ---
-#print(to_xml(page))
-
-# ---
-app,rt = fast_app()
-
-@rt('/')
-def get(): return page
+@rt("/")
+def get():
+    return Title("FastHTML"), H1("Hello World!")
   
-serve()
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", default=8080)))
