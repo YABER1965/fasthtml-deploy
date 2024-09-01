2bc228c7aeb5104b2bc1b19ed67f597a41ca713a
Ryxjx YxBx <51150175+YABER1965@users.noreply.github.com>
2024-09-01 15:22:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+#from fasthtml.common import *
+
+#app, rt = fast_app()
+
+
+#@rt("/")
+#def get():
+#    return Div(P("Hello World!"), hx_get="/change")
+
+#serve()
+
+# ---
 from fasthtml.common import *
+import uvicorn
 
-app, rt = fast_app()
-
+app = FastHTML()
+rt = app.route
 
 @rt("/")
 def get():
-    return Div(P("Hello World!"), hx_get="/change")
-
-
-serve()
+    return Title("FastHTML"), H1("Hello World!")
+  
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", default=8000)))
